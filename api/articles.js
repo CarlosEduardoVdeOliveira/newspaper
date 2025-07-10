a6088bc54d78e6c67712c05645f4612f903fab64
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2025-07-10 14:15:02 -0300

    update vercel

--- api/articles.js
+++ api/articles.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL_API =
   "https://newsapi.org/v2/everything?q=keyword&apiKey=90b0a1e6642449088471333a43bcb4c2";
 
-export default async function handler(res) {
+export default async function handler(req, res) {
   try {
     const response = await axios.get(URL_API);
     res.status(200).json(response.data);
